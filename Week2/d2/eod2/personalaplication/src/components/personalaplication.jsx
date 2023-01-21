378eccb860f232649c8e1fa09bddcf3c0b073a2c
CuongPQuach <geminiquach@gmail.com>
2023-01-21 10:03:03 -0800

    Week2 eod 2-3

--- Week2/d2/eod2/personalaplication/src/components/personalaplication.jsx
+++ Week2/d2/eod2/personalaplication/src/components/personalaplication.jsx
@@ -5,36 +5,120 @@ const Personalaplication = (props) => {
         fname:'',
         lname:'',
         email:'',
-        password:'',
+        pass:'',
         confirmpass:''
     })
     
+    const [memberlist,setmemberlist] = useState([])
+
+
+
+
+
     const onChangehandler = (e) => {
         setpersonalcard(prevState => {return{...prevState,[e.target.name]:e.target.value}})
     }
 
+    const runValidations =() =>{
+        let isValid = true
+        if (personalcard.fname.length <2 || personalcard.lname.length <2 || personalcard.email.length <2 || personalcard.pass.length <8 || personalcard.pass !== personalcard.confirmpass){
+        console.log('Please Check Requirement')
+        isValid = false
+        return isValid
+        }
+        else{
+            return isValid
+        }
+    }
+
+
+
+
+    const onSubmithandler = (e) => {
+        e.preventDefault();
+        // $$$$$$$ sumit with out Validation
+        // console.log('NewMember:' + JSON.stringify(personalcard))
+        //  back tick key (before number 1)
+        // console.log(`NewMember: ${JSON.stringify(personalcard)}`)
+        // ######## submit with validation
+        let isValid = runValidations();
+        console.log(isValid)
+        if (isValid){
+            console.log(`NewMember: ${JSON.stringify(personalcard)}`)
+            setmemberlist([...memberlist,personalcard])
+            setpersonalcard({
+            fname:'',
+            lname:'',
+            email:'',
+            pass:'',
+            confirmpass:''
+            })
+        }
+        else {
+            return isValid
+        }
+
+
+
+    }
     return(
-        <div >
-            <form className='d-flex'> 
+        <>
+        <div  >
+            <form onSubmit={onSubmithandler}> 
                 <label htmlFor="fname"> First Name: </label>
                 <input type="text" name="fname" onChange={onChangehandler} />
+                {
+                    personalcard.fname.length <2?
+                    <p>First Name must at least 2 Character</p>:
+                    null
+                }
                 <label htmlFor="lname"> Last Name: </label>
                 <input type="text" name="lname" onChange={onChangehandler} />
-                <label htmlFor="email"> First Name: </label>
+                {
+                    personalcard.lname.length <2?
+                    <p>Last Name must at least 2 Character</p>:
+                    null
+                }
+                <label htmlFor="email"> Email: </label>
                 <input type="text" name="email" onChange={onChangehandler} />
-                <label htmlFor="password"> Password: </label>
-                <input type="password"  name="password" onChange={onChangehandler} />
+                {
+                    personalcard.email.length <2?
+                    <p>Email must at least 2 Character</p>:
+                    null
+                }
+                <label htmlFor="pass"> Password: </label>
+                <input type="password"  name="pass" onChange={onChangehandler} />
+                {
+                    personalcard.pass.length <8?
+                    <p>Password must at least 8 Character</p>:
+                    null
+                }
                 <label htmlFor="confirmpass"> Confirm Password: </label>
                 <input type="password"  name="confirmpass" onChange={onChangehandler}/>
-
+                {
+                    personalcard.confirmpass !== personalcard.pass?
+                    <p>Passowrd must at matching confirm password</p>:
+                    null
+                }
+                <button>Register</button>
             </form>
-            <h2>First Name: {personalcard.fname}</h2>
+            {/* <h2>First Name: {personalcard.fname}</h2>
             <h2>Last Name : {personalcard.lname}</h2>
             <h2>Email : {personalcard.email}</h2>
-            <h2>Password : {personalcard.password}</h2>
-            <h2>Confirm Password :{personalcard.confirmpass}</h2>
+            <h2>Password : {personalcard.pass}</h2>
+            <h2>Confirm Password :{personalcard.confirmpass}</h2> */}
         </div>
-    )
-}
+        {
+            memberlist.map((personalcard) => (
+                <div>
+                <p>First Name:{personalcard.fname}</p>
+                <p>Last Name:{personalcard.lname}</p>
+                <p>Email:{personalcard.email}</p>
+                <p>Password:{personalcard.pass}</p>
+                </div>
+                ))
+        }
+        </>
+    )}
 
 export default Personalaplication;
